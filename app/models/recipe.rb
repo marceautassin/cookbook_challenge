52b9fd3f6d2962e51fedbbd6ec734b346fd84173
Marceau Tassin <marceau.tassin@gmail.com>
2020-04-27 23:48:18 +0200

    index recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,15 @@
 class Recipe < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :name, :time_prep, :difficulty ],
+    associated_against: {
+      ingredient: [ :name, :category ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
-  get 'doses/destroy'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'recipes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :recipes do
     resources :doses, only: %w[new create destroy], shallow: true
