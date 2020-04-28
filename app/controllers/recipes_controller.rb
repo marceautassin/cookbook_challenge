d27dcc4834e5dc903d5f2c684ac99bb88aa2c021
Marceau Tassin <marceau.tassin@gmail.com>
2020-04-28 22:03:33 +0200

    index + show ok

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
 
-  before_action :set_recipe ,only: %i[show create edit update destroy]
+  before_action :set_recipe ,only: %i[show edit update destroy]
 
   def index
     @recipes = Recipe.all
